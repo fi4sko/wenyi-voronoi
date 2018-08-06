adfee6012246b4f7edb421955b80acdd782d1bea
fi4sko <jedrzej.szymanski@googlemail.com>
2018-08-06 11:18:23 +0200

    Add data

--- script_voronoi_arabidopsis.R
+++ script_voronoi_arabidopsis.R
@@ -46,10 +46,7 @@ bins.description = as.vector(unlist(sapply(names(bins),function(x) mapman[mapman
 names(bins.description) = names(bins)
 rm(list=ls(pattern="tmp"))
 
-bins = c(unique(unlist(bins)), bins)
 
-bins.description[1] = "root"
-names(bins.description)[1] = "bin_R."
 
 
 
@@ -59,13 +56,13 @@ rm(list=ls(pattern="tmp"))
 
 ### add hand edited short names
 bins.description2 = bins.description
-load('shortnames')
-names(shortnames) = gsub("bin_", "bin_R.", names(shortnames), fixed = T)
-names(shortnames) = gsub("bin_R.R.", "bin_R.", names(shortnames), fixed = T)
-shortnames = shortnames[names(bins.description2)]
-
-tmp = cbind(names(bins.description2), bins.description2, shortnames)
-write.table(tmp, "shortnames_edit.txt", sep = "\t")
+# load('shortnames')
+# names(shortnames) = gsub("bin_", "bin_R.", names(shortnames), fixed = T)
+# names(shortnames) = gsub("bin_R.R.", "bin_R.", names(shortnames), fixed = T)
+# shortnames = shortnames[names(bins.description2)]
+# 
+# tmp = cbind(names(bins.description2), bins.description2, shortnames)
+# write.table(tmp, "shortnames_edit.txt", sep = "\t")
 #manual edit
 tmp = read.csv("shortnames_edit.txt", header = T, row.names = 1,sep = "\t", quote = "")
 shortnames = as.vector(tmp$shortnames)
@@ -90,6 +87,40 @@ for(i in 1:length(bins.description2)){
   bins.description2[i] = f1(bins.description[i], shortnames2[i])
 }
 
+
+
+### create list of terms
+# O1 <- c("bin_R." = list(unique(unlist(bins))),bins)
+# #names(O1) <- c("R","T11","T12")
+# O1 = O1[c(1,3:length(O1))] # remove the empty root
+# O1 = lapply(O1, function(x) toupper(substr(x[grep("solyc", x)], 1,14)))
+O1 = bins[lapply(O1,length) > 0]
+
+### load the data
+
+tmp = sapply(dir("data_arabidopsis/"), function(x) read.csv(paste("data_arabidopsis/", x, sep = ""), header = T, row.names = 1, sep = "\t"), simplify = F)
+
+data1 = read.csv("data_arabidopsis/comparsion 1.csv", header = T, row.names = 1, sep = "\t")
+data2 = read.csv("data_arabidopsis/comparsion 2.csv", header = T, row.names = 1, sep = "\t")
+data3 = read.csv("data_arabidopsis/comparsion 3.csv", header = T, row.names = 1, sep = "\t")
+data4 = read.csv("data_arabidopsis/comparsion 4.csv", header = T, row.names = 1, sep = "\t")
+
+Y.names = unique(c(rownames(data1),rownames(data2),rownames(data3),rownames(data4)))
+Y = matrix(0, nrow = length(Y.names), ncol = 4, dimnames = list(Y.names, paste("comparison",1:4, sep = "_")))
+Y[rownames(data1),1] = as.numeric(as.vector(data1[,1]))
+Y[rownames(data2),2] = as.numeric(as.vector(data2[,1]))
+Y[rownames(data3),3] = as.numeric(as.vector(data3[,1]))
+Y[rownames(data4),4] = as.numeric(as.vector(data4[,1]))
+
+Y[is.na(Y)] = 0
+Y[Y == Inf] = max(Y[Y != Inf]) + 1
+Y[Y == -Inf] = min(Y[Y != -Inf]) - 1
+
+
+### filter the hierarchy
+
+O2 = lapply(O1, function(x) x[x %in% rownames(Y)]) 
+O2 = O2[unlist(lapply(O2, length)) > 0]
 
 
 
